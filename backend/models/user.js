4a3da8e951e0c3bef49ed16f214a545e16cfe7e6
nuwani-sithara <it22099518@my.sliit.lk>
2025-09-02 14:47:13 +0530

    added user model

---
+++ backend/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    trim: true,
+    enum: ['student', 'instructor']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
