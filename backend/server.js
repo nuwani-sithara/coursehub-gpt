efda265b8e5b23ff49f70fe55df5dbfe3ca31e9f
nuwani-sithara <it22099518@my.sliit.lk>
2025-09-02 14:48:24 +0530

    added server.js for the mongodb connection and connection between routes

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB connected');
+});
+
+const userRoutes = require('./routes/userRoute.js');
+app.use('/users', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
